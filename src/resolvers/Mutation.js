4ed3365210f436ebf726e6f829a2f61705812a0b
GenoDiaz <genocdiaz@gmail.com>
2018-10-04 00:15:56 -0700

    Initialize current progress

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,8 @@
+const Mutations = {
+  createDog(parent, args, cta, info) {
+    // create a dog
+    console.log();
+  }
+};
+
+module.exports = Mutations;
---
+++ src/resolvers/Query.js
@@ -0,0 +1,7 @@
+const Query = {
+  dogs(parent, args, ctx) {
+    return [{ name: 'Snickers' }, { name: 'Sunny' }];
+  },
+};
+
+module.exports = Query;
